8f77bc8cfe1698d38c7d19be6d39dc45a5c9a141
LauraMarques20 <laura.lima.marques2002@hotmail.com>
2024-09-21 15:58:41 -0300

    crud de categoria

--- categoria/admin.py
+++ categoria/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 from .models import Categoria
 
-admin.site.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'descricao') 
+
+admin.site.register(Categoria, CategoriaAdmin)

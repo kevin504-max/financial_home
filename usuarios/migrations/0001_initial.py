6122348d884f87c8dd953ae5e13d58b7cfc0827a
kevin504-max <kevinbrito2012@gmail.com>
2024-09-22 12:06:19 -0300

    chore: include created_at field in users migration

--- usuarios/migrations/0001_initial.py
+++ usuarios/migrations/0001_initial.py
@@ -17,6 +17,7 @@ class Migration(migrations.Migration):
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('nome', models.CharField(max_length=100)),
                 ('email', models.EmailField(max_length=254)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
             ],
         ),
     ]
